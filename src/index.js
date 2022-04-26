733e67862f2aa2ade4836ec8228583d5ccc53011
jeffreyjv <jeffjv@live.unc.edu>
2022-04-26 17:32:49 -0400

    login bug fix

--- src/index.js
+++ src/index.js
@@ -13,6 +13,3 @@ ReactDOM.render(
 );
 reportWebVitals();
 
-const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App />);
